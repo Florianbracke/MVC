18d60884c6884c2889e79002a8572f1520863678
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-02-03 11:54:49 +0100

    Form submit button styling

--- closet.php
+++ closet.php
@@ -19,57 +19,72 @@ require 'view/includes/header.php'?>
 
 <?=$alert?>
 <div class="grid-container"> 
-        <?php foreach ($items as $item) : ?>
-        <div class="grid-item"> 
-            <button class="accordion">
-                <img src=images/<?= $item['image'] ?>>
-            </button>
-            <div class="panel">
-            <p> Type of Item: <?= ucfirst($item['type']) ?> </p>
-            <p> Weather Type: <?= ucfirst($item['weather']) ?> </p>
-            <p> Occasion: <?= ucfirst($item['ocassion']) ?> </p>
-            <p> Colour: <?= ucfirst($item['colour']) ?> </p>
-            <p> Time of Day: <?= ucfirst($item['time']) ?> </p>
-            <p> <?=$item['favorite'] ?> </p>
-            </button>
-                <form method="post">
-                    <input type="hidden" name="item_type" value="<?=$item['type'] ?>">
-                    <input type="hidden" name="item_id" value="<?=$item['id'] ?>">
-                    <input type="hidden" name="item_image" value="<?=$item['image'] ?>">
-                    <input type="hidden" name="item_weather" value="<?=$item['weather'] ?>">
-                    <input type="hidden" name="item_ocassion" value="<?=$item['ocassion'] ?>">
-                    <input type="hidden" name="item_colour" value="<?=$item['colour'] ?>">
-                    <input type="hidden" name="item_time" value="<?=$item['time'] ?>">
-                    <input type="submit" name="add_to_outfit" value="WEAR">
-                </form>
-                <form action="editItem.php?edit=<?=$item['id']?>" method="post">
-                    <input type="hidden" name="item_type" value="<?=$item['type'] ?>">
-                    <input type="hidden" name="item_id" value="<?=$item['id'] ?>">
-                    <input type="hidden" name="item_image" value="<?=$item['image'] ?>">
-                    <input type="hidden" name="item_weather" value="<?=$item['weather'] ?>">
-                    <input type="hidden" name="item_ocassion" value="<?=$item['ocassion'] ?>">
-                    <input type="hidden" name="item_colour" value="<?=$item['colour'] ?>">
-                    <input type="hidden" name="item_time" value="<?=$item['time'] ?>">
-                    <input type="submit" name="edit_item" value="EDIT">
-                </form>
-                <form action="deleteItem.php?delete=<?=$item['id']?>" method="post">
-                    <input type="submit" name="delete_item" value="DELETE">
-                </form>
-                <form action="favorites.php?favorite=<?=$item['id']?>" method="post">
-                    <input type="submit" name="add_favorite" value="Add favorite">
-                </form>
-                <form action="inspiration.php?item=<?=$item['id']?>" method="post">
-                    <input type="hidden" name="item_type" value="<?=$item['type'] ?>">
-                    <input type="hidden" name="item_image" value="<?=$item['image'] ?>">
-                    <input type="hidden" name="item_weather" value="<?=$item['weather'] ?>">
-                    <input type="hidden" name="item_ocassion" value="<?=$item['ocassion'] ?>">
-                    <input type="hidden" name="item_colour" value="<?=$item['colour'] ?>">
-                    <input type="hidden" name="item_time" value="<?=$item['time'] ?>">
-                    <input type="submit" name="get_inspiration" value="Get inspiration">
-                </form>
+    <?php foreach ($items as $item) : ?>
+    <div class="grid-item"> 
+        <button class="accordion">
+            <img src=images/<?= $item['image'] ?>>
+        </button>
+        <div class="panel">
+            <ul>
+                <li> <?= ucfirst($item['type']) ?> | </li>
+                <li> <?= ucfirst($item['weather']) ?> weather | </li>
+                <li> <?= ucfirst($item['ocassion']) ?> |</li>
+                <li> <?= ucfirst($item['colour']) ?>| </li>
+                <li> <?= ucfirst($item['time']) ?> | </li>
+                <li> <?=$item['favorite'] ?> </li>
+            </ul>
+            <div class="inline-buttons">
+            <ul>
+                <li>
+                    <form method="post">
+                        <input type="hidden" name="item_type" value="<?=$item['type'] ?>">
+                        <input type="hidden" name="item_id" value="<?=$item['id'] ?>">
+                        <input type="hidden" name="item_image" value="<?=$item['image'] ?>">
+                        <input type="hidden" name="item_weather" value="<?=$item['weather'] ?>">
+                        <input type="hidden" name="item_ocassion" value="<?=$item['ocassion'] ?>">
+                        <input type="hidden" name="item_colour" value="<?=$item['colour'] ?>">
+                        <input type="hidden" name="item_time" value="<?=$item['time'] ?>">
+                        <input type="submit" name="add_to_outfit" value="Wear" class="button-closet">
+                    </form>
+                </li>
+                <li>
+                    <form action="favorites.php?favorite=<?=$item['id']?>" method="post">
+                        <input type="submit" name="add_favorite" value="&#xf004;" class="button-closet">
+                    </form>
+                </li>
+                <li>
+                    <form action="inspiration.php?item=<?=$item['id']?>" method="post">
+                        <input type="hidden" name="item_type" value="<?=$item['type'] ?>">
+                        <input type="hidden" name="item_image" value="<?=$item['image'] ?>">
+                        <input type="hidden" name="item_weather" value="<?=$item['weather'] ?>">
+                        <input type="hidden" name="item_ocassion" value="<?=$item['ocassion'] ?>">
+                        <input type="hidden" name="item_colour" value="<?=$item['colour'] ?>">
+                        <input type="hidden" name="item_time" value="<?=$item['time'] ?>">
+                        <input type="submit" name="get_inspiration" value="Get inspired" class="button-closet">
+                    </form>
+                </li>
+                <li>
+                    <form action="editItem.php?edit=<?=$item['id']?>" method="post">
+                        <input type="hidden" name="item_type" value="<?=$item['type'] ?>">
+                        <input type="hidden" name="item_id" value="<?=$item['id'] ?>">
+                        <input type="hidden" name="item_image" value="<?=$item['image'] ?>">
+                        <input type="hidden" name="item_weather" value="<?=$item['weather'] ?>">
+                        <input type="hidden" name="item_ocassion" value="<?=$item['ocassion'] ?>">
+                        <input type="hidden" name="item_colour" value="<?=$item['colour'] ?>">
+                        <input type="hidden" name="item_time" value="<?=$item['time'] ?>">
+                        <input type="submit" name="edit_item" value="&#xf044;" class="button-closet">
+                    </form>
+                </li>
+                <li>
+                    <form action="deleteItem.php?delete=<?=$item['id']?>" method="post">
+                        <input type="submit" name="delete_item" value="&#xf1f8;" class="button-closet trash">
+                    </form>
+                </li>
+            </ul>
             </div>
         </div>
-        <?php endforeach; ?>
+    </div>
+    <?php endforeach; ?>
 </div>
 
 </div>
