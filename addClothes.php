3f9a4887b2660b62d069aa37dd322b90c8aaed2c
Florianbracke <florianbracke@gmail.com>
2021-02-03 16:29:01 +0100

    add correct link to navbar

--- addClothes.php
+++ addClothes.php
@@ -14,7 +14,7 @@ error_reporting(E_ALL);
     // set the PDO error mode to exception
     $database->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  } catch(PDOException $e) {
+    } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     }
 
@@ -29,7 +29,7 @@ error_reporting(E_ALL);
       $query = "INSERT INTO images (name,image) VALUES(?,?)";
     
       $statement = $database->prepare($query);
-    
+      
       // Loop all files
       for($i=0;$i<$countfiles;$i++){
 
@@ -67,6 +67,9 @@ error_reporting(E_ALL);
       }
     }
 
+    
+    // $favorites = $database->query("SELECT name FROM TABLE images WHERE;");
+
 
  
 ?>
@@ -97,6 +100,89 @@ error_reporting(E_ALL);
     <input type='file' name='files[]' multiple /> <br>
     <input type='submit' value='Submit' name='submit' />
   </form>
+
+
+
+  <div class="container"> 
+ 
+        <?php foreach ($items as $item) : ?>
+                <img src=images/<?= $item['image'] ?>>
+                <div class="edit-text"> 
+                <form method="post">
+                    <label for="item_type"> Type of item: </label>
+                    <select name="item_type" class="custom-select">
+                   
+                        <option value="outer wear">Outer Wear</option>
+                        <option value="pants">Pants</option>
+                        <option value="skirts">Skirts</option>
+                        <option value="jeans">Jeans</option>
+                        <option value="dress">Dress</option>
+                        <option value="shorts">Shorts</option>
+                        <option value="blouse">Blouse</option>
+                        <option value="knits">Knits</option>
+                        <option value="tshirt">T-shirt</option>
+                        <option value="jewelry">Jewelry</option>
+                        <option value="glasses">Glasses</option>
+                        <option value="accessory">Accessory</option>
+                        <option value="sneakers">Sneakers</option>
+                        <option value="heels">Heels</option>
+                        <option value="shoes">Shoes</option>
+                        <option value="hat">Hats</option>
+                    </select> <br>
+                    <label for="item_weather"> Weather type: </label>
+                    <select name="item_weather" class="custom-select">
+                    
+                        <option value="hot">Hot</option>
+                        <option value="cold">Cold</option>
+                        <option value="rain">Rain</option>
+                        <option value="all">All</option>
+                    </select> <br>
+                    <label for="item_ocassion"> Ocassion: </label>
+                    <select name="item_ocassion" class="custom-select">
+                 
+                        <option value="casual">Casual</option>
+                        <option value="classy">Classy</option>
+                        <option value="party">Party</option>
+                        <option value="sporty">Sporty</option>
+                        <option value="business">Business</option>
+                        <option value="beach">Beach</option>
+                    </select>   <br>                 
+                    <label for="item_colour"> Colour: </label>
+                    <select name="item_colour" class="custom-select">
+         
+                        <option value="red">Red</option>
+                        <option value="orange">Orange</option>
+                        <option value="yellow">Yellow</option>
+                        <option value="green">Green</option>
+                        <option value="blue">Blue</option>
+                        <option value="purple">Purple</option>
+                        <option value="black">Black</option>
+                        <option value="white">White</option>
+                        <option value="pink">Pink</option>
+                        <option value="off white">Off White</option>
+                        <option value="grey">Grey</option>
+                        <option value="print">Print</option>
+                    </select>   <br>                  
+                    <label for="item_time"> Time of Day: </label>
+                    <select name="item_time" class="custom-select">
+                       
+                        <option value="day">Day</option>
+                        <option value="evening">Evening</option>
+                        <option value="night">Night</option>
+                    </select>   
+                    <br> 
+                    <br> 
+                    <input type="submit" name="edit_confirm" value="Edit" class="button-closet editPageButton">
+                </form>
+                <form action="closet.php" method="post">
+                    <input type="submit" value="Cancel" class="button-closet editPageButton">
+                </form>
+        </div>
+        <?php endforeach; ?>
+</div>
+
+
+
 </div>
 
 <div class="targetfile">
--- closet.php
+++ closet.php
@@ -8,7 +8,7 @@ require 'view/includes/header.php'?>
 <div class="topnav">
   <h3>Your Closet</h3> 
   <ul>
-    <li><a href="upload.php"><i class="fas fa-file-upload"></i></a></li>
+    <li><a href="addClothes.php"><i class="fas fa-file-upload"></i></a></li>
     <li><a href="outfit.php"><i class="fas fa-calendar-day"></i></a></li>
     <li><a href="favorites.php"><i class="fas fa-heart"></i></a></li>
   </ul>
