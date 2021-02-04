da96d5407629d994b826070db25d4527c4e19987
VidyashreeTarikere <tvvidyashree@gmail.com>
2021-02-04 14:09:27 +0100

    login files

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php //include("includes/header.php"); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Digital Closet</title>
+</head>
+
+<body>
+  <section class="showcase">
+    <header>
+      <h2 class="logo">Digital Closet</h2>
+      <div class="toggle"></div>
+    </header>
+
+    <video src="explore.mp4" muted loop autoplay></video>
+    <div class="overlay"></div>
+
+    <div class="text">
+      <h2>You can't reach for new clothes, </h2>
+      <h3>If your hands are still full of yesterday’s clothes</h3>
+      <p>Now put everything back together in a way that makes sense to you, utilizes all of your available space, and is attractive enough to motivate closet maintenance. Think about where it makes sense to store each category of clothing and then think about the easiest spots to reach in your closet.</p>
+    </div>
+
+    <ul class="social">
+      <li><a href="#"><img src="https://i.ibb.co/x7P24fL/facebook.png"></a></li>
+      <li><a href="#"><img src="https://i.ibb.co/Wnxq2Nq/twitter.png"></a></li>
+      <li><a href="#"><img src="https://i.ibb.co/ySwtH4B/instagram.png"></a></li>
+    </ul>
+  </section>
+
+  <div class="menu">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="aboutUs.php">About Us</a></li>
+      <li><a href="#">Digital closet</a></li>
+      <li><a href="musicPlayer/music.php" target="_blank" rel="noopener noreferrer">Music Player</a></li>
+      <li><a href="register.php">Register</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+  </div>
+  <script src="scriptLandingPage.js"></script>
+</body>
+
+</html>
