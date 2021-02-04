96c567de8a8cd3cbb80ee000411827f2fd59c0f8
VidyashreeTarikere <tvvidyashree@gmail.com>
2021-02-04 23:34:11 +0100

    pushing about us page

---
+++ View/aboutUs.php
@@ -0,0 +1,85 @@
+<?php include("includes/headerIndex.php"); ?>
+
+<?php include("includes/navIndex.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="aboutUs.css">
+    <title>About Us</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="imgBox">
+                <img src="./aboutUsPics/florian.jpg" alt="">
+            </div>
+            <div class="content">
+                <h2>Florian B</h2>
+                <p>Junior Web Developer</p>
+                <p>entrepreneur, Digital Closets</p>
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-linkedin"></a>
+                <a href="#" class="fa fa-instagram"></a>
+            </div>
+
+        </div>
+
+        <div class="card">
+            <div class="imgBox">
+                <img src="./aboutUsPics/fran.jpg" alt="">
+            </div>
+            <div class="content">
+                <h2>Fran Andries</h2>
+                <p>Junior Web Developer</p>
+                <p>entrepreneur, Digital Closets</p>
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-linkedin"></a>
+                <a href="#" class="fa fa-instagram"></a>
+            </div>
+
+        </div>
+
+        <div class="card">
+            <div class="imgBox">
+                <img src="./aboutUsPics/maureen.jpg" alt="">
+            </div>
+            <div class="content">
+                <h2>Maureen Oldyck</h2>
+                <p>Junior Web Developer</p>
+                <p>entrepreneur, Digital Closets</p>
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-linkedin"></a>
+                <a href="#" class="fa fa-instagram"></a>
+            </div>
+
+        </div>
+
+        <div class="card">
+            <div class="imgBox">
+                <img src="./aboutUsPics/vidya.jpg" alt="">
+            </div>
+            <div class="content">
+                <h2>Vidya Tarikere</h2>
+                <p>Junior Web Developer</p>
+                <p>entrepreneur, Digital Closets</p>
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-linkedin"></a>
+                <a href="#" class="fa fa-instagram"></a>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
